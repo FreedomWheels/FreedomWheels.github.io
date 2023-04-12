05836d56b69fae1bbfee9928c255dac941efc200
Freedom Wheels <freedomwheelsdrivingschool@gmail.com>
2023-04-12 11:14:41 -0400

    index.php and form-handler.php moved to root directory

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Submission Success</title>
+</head>
+<body>
+	<h1>Form Submitted Successfully</h1>
+	<p>Thank you for contacting us. We will get back to you as soon as possible.</p>
+</body>
+</html>
