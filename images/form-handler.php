4f8c9bd9f66525e5ddf3651a928a959abca1e94f
Freedom Wheels <freedomwheelsdrivingschool@gmail.com>
2023-04-12 10:57:36 -0400

    PHP Code Update

--- images/form-handler.php
+++ images/form-handler.php
@@ -2,10 +2,12 @@
 
 $to = 'freedomwheelsdrivingschool@gmail.com';
 $subject = 'New message from website';
-$message = $_POST['message'];
-$headers = 'From: ' . $_POST['email'];
+$message = $_GET['message'];
+$headers = 'From: ' . $_GET['email'];
 
 mail($to, $subject, $message, $headers);
+
+header("Location: thank-you.html");
 
 ?>
 
