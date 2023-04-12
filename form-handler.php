ef0dea487991a2d1856b8c1c867f49e287048dcc
Freedom Wheels <freedomwheelsdrivingschool@gmail.com>
2023-04-12 11:32:04 -0400

    Minor php updates for form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,13 +1,12 @@
 <?php
+if(isset($_GET['submit'])){
+    $to = 'freedomwheelsdrivingschool@gmail.com';
+    $subject = 'New message from website';
+    $message = $_GET['message'];
+    $headers = 'From: ' . $_GET['email'];
 
-$to = 'freedomwheelsdrivingschool@gmail.com';
-$subject = 'New message from website';
-$message = $_GET['message'];
-$headers = 'From: ' . $_GET['email'];
+    mail($to, $subject, $message, $headers);
 
-mail($to, $subject, $message, $headers);
-
-header("Location: thank-you.html");
-
+    header("Location: thank-you.html");
+}
 ?>
-
